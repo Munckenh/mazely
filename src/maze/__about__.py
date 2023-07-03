1a6a0cb88666416263fff3be3a86974391e8c5d8
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-03 17:45:52 +0800

    Treat `src/maze/` as a package, add metadata

---
+++ src/maze/__about__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+__author__ = "Kenneth C."
+__copyright__ = f"Copyright (c) 2023 {__author__}"
+__license__ = "MIT License"
---
+++ src/maze/__init__.py
@@ -0,0 +1,1 @@
+from .__about__ import __version__, __author__, __copyright__, __license__
