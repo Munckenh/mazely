535de58108f907793f9e4681ce9b13015f35f026
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-06 18:45:20 +0800

    Single-source the package version

--- src/maze/__about__.py
+++ src/maze/__about__.py
@@ -1,4 +1,6 @@
-__version__ = "0.1.0"
+import importlib.metadata
+
+__version__ = importlib.metadata.version("maze.py")
 __author__ = "Kenneth C."
 __copyright__ = f"Copyright (c) 2023 {__author__}"
 __license__ = "MIT License"
