e3bc3cb726d460e34ac7194d3d45c79efa87c18b
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-12 22:21:34 +0800

    Add a raise statement to `set_goal_cell()`

--- src/mazely/maze.py
+++ src/mazely/maze.py
@@ -162,7 +162,16 @@ class Maze:
             The row of a cell.
         column : int
             The column of a cell.
+
+        Raises
+        ------
+        ValueError
+            If either row or column is out of range. 
         """
+        if row < 0 or row >= self.rows:
+            raise ValueError("Row is out of range.")
+        if column < 0 or column >= self.columns:
+            raise ValueError("Column is out of range.")
         self.goal = {(row, column)}
 
     def add_goal_cell(self, row: int, column: int):
