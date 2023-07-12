d90e789b4d18629b4da074c14988bb26b8a95e24
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-13 06:14:08 +0800

    Create a test suite

---
+++ tests/test_utilities.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+
+import numpy as np
+import pytest
+
+from mazely import Maze, Utilities
+
+
+def test_show_grid(utilities: Utilities, grid: np.ndarray):
+    pass
+
+
+def test_save_grid(utilities: Utilities, grid: np.ndarray, tmp_path: Path):
+    pass
+
+
+def test_show_solution(utilities: Utilities, maze: Maze):
+    pass
+
+
+def test_save_solution(utilities: Utilities, tmp_path: Path):
+    pass
