06dacd317e873640668b0f02ded959dc141d3298
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-17 04:51:25 +0800

    Update the test suite

---
+++ tests/baseline/_hash.py
@@ -0,0 +1,46 @@
+"""Prints out the hash of the baseline images for `mazely.Utilities.save_grid()` and `mazely.Utilities.save_solution()`"""
+
+import hashlib
+import os
+import tempfile
+
+from mazely import Maze, Utilities
+
+try:
+    import cairosvg
+except OSError:
+    import os
+    from ctypes.util import find_library
+    library_names = ("cairo-2", "cairo", "libcairo-2")
+    for library_name in library_names:
+        library_path = find_library(library_name)
+        if library_path:
+            os.add_dll_directory(os.path.dirname(library_path))
+    import cairosvg
+
+
+def svg_to_hash256(file_path: str, verbose: bool = True, _debug: bool = False) -> str:
+    file_bytes = cairosvg.svg2png(url=file_path, background_color="#FFF")
+    if _debug:
+        output = f"{os.path.basename(os.path.splitext(file_path)[0])}.png"
+        cairosvg.svg2png(url=file_path, write_to=output,
+                         background_color="#FFF")
+    file_hash = hashlib.sha256(file_bytes).hexdigest()
+    if verbose:
+        print(file_hash)
+    return file_hash
+
+
+if __name__ == "__main__":
+    maze = Maze(3, 3, seed=0)
+    utilities = Utilities()
+    maze.set_start_cell(0, 0)
+    maze.set_goal_cell(1, 1)
+    maze.solve()
+    with tempfile.TemporaryDirectory() as tempdir:
+        grid_svg = os.path.join(tempdir, "grid.svg")
+        solution_svg = os.path.join(tempdir, "solution.svg")
+        utilities.save_grid(maze.grid, grid_svg)
+        utilities.save_solution(maze.grid, maze.solution_path, solution_svg)
+        svg_to_hash256(grid_svg, verbose=True)
+        svg_to_hash256(solution_svg, verbose=True)
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,35 +1,42 @@
+import json
+
 import pytest
 
 from mazely import Maze, Utilities
-from mazely.algorithms import RecursiveBacktracking
+
+
+# The generated maze:
+#     +---+---+---+
+#     | S |       |
+#     +   +   +   +
+#     |   | G |   |
+#     +   +---+   +
+#     |           |
+#     +---+---+---+
+@pytest.fixture
+def maze():
+    maze_ = Maze(3, 3, seed=0)
+    maze_.set_start_cell(0, 0)
+    maze_.set_goal_cell(1, 1)
+    maze_.solve()
+    return maze_
+
+
+@pytest.fixture
+def grid(maze):
+    return maze.grid
+
+
+@pytest.fixture
+def solution_path(maze):
+    return maze.solution_path
 
 
 @pytest.fixture
 def utilities():
     return Utilities()
 
-
 @pytest.fixture
-def grid():
-    generator = RecursiveBacktracking()
-    # +---+---+---+
-    # |   |       |
-    # +   +   +   +
-    # |   |   |   |
-    # +   +---+   +
-    # |           |
-    # +---+---+---+
-    return generator.generate(3, 3, seed=0)
-
-
-@pytest.fixture
-def maze():
-    # maze.grid:
-    #   +---+---+---+
-    #   |   |       |
-    #   +   +   +   +
-    #   |   |   |   |
-    #   +   +---+   +
-    #   |           |
-    #   +---+---+---+
-    return Maze(3, 3, seed=0)
+def hashes():
+    with open("tests/baseline/hashes.json", "r") as file:
+        return json.load(file)
