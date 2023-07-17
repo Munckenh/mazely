bb1f9580b12799a21a527312f45361b6dc48f995
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-17 20:32:21 +0800

    Set the `__all__` variable

--- src/mazely/__init__.py
+++ src/mazely/__init__.py
@@ -2,3 +2,13 @@ from . import algorithms
 from .maze import Maze
 from .utilities import Utilities
 from .__about__ import __version__, __author__, __copyright__, __license__
+
+__all__ = [
+    "algorithms",
+    "Maze",
+    "Utilities",
+    "__version__",
+    "__author__",
+    "__copyright__",
+    "__license__"
+]
