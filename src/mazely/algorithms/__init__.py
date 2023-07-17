f17edb691ef18af9408a8305556e1d9ecea744b1
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-17 20:32:21 +0800

    Set the `__all__` variable

--- src/mazely/algorithms/__init__.py
+++ src/mazely/algorithms/__init__.py
@@ -2,3 +2,10 @@ from .maze_generator import MazeGenerator
 from .maze_solver import MazeSolver
 from .recursive_backtracking import RecursiveBacktracking
 from .shortest_path import ShortestPath
+
+__all__ = [
+    "MazeGenerator",
+    "MazeSolver",
+    "RecursiveBacktracking",
+    "ShortestPath"
+]
