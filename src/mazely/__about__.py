5d9a48f65b7df6b88df8602ed084f80671f10279
Munckenh <76769232+Munckenh@users.noreply.github.com>
2023-07-21 22:48:38 +0800

    Update import statement for version retrieval

--- src/mazely/__about__.py
+++ src/mazely/__about__.py
@@ -1,6 +1,6 @@
-import importlib.metadata
+from importlib import metadata
 
-__version__ = importlib.metadata.version("mazely")
+__version__ = metadata.version("mazely")
 __author__ = "Kenneth C."
 __copyright__ = f"Copyright (c) 2023 {__author__}"
 __license__ = "MIT License"
